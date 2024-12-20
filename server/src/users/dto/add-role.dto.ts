0faea850b035008440cef64181e1ddd461de7ea1
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2024-12-20 19:00:21 +0200

    Add validation for add-role.dto.ts

--- server/src/users/dto/add-role.dto.ts
+++ server/src/users/dto/add-role.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger"
+import { IsNumber, IsString } from "class-validator"
 
 export class AddRoleDto {
   @ApiProperty({ example: "ADMIN", description: "Role value" })
+  @IsString({ message: "Should be a string" })
   readonly value: string
 
   @ApiProperty({ example: 1, description: "User identifier" })
+  @IsNumber({}, { message: "Should be a number" })
   readonly userId: number
 }
