c7150e4702e8e00f4ac87b68151748d367ed5da9
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2024-12-20 16:45:48 +0200

    Add roles to swagger

--- server/src/roles/dto/create-role.dto.ts
+++ server/src/roles/dto/create-role.dto.ts
@@ -1,3 +1,6 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class CreateRoleDto {
+  @ApiProperty({ example: "ADMIN", description: "Role value" })
   readonly value: string
 }
--- server/src/roles/roles.controller.ts
+++ server/src/roles/roles.controller.ts
@@ -1,17 +1,23 @@
 import { Body, Controller, Get, Param, Post } from "@nestjs/common"
 import { RolesService } from "./roles.service"
 import { CreateRoleDto } from "./dto/create-role.dto"
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger"
 
+@ApiTags("Roles")
 @Controller("roles")
 export class RolesController {
   constructor(private rolesService: RolesService) {
   }
 
+  @ApiOperation({ summary: "Create role" })
+  @ApiResponse({ status: 200, type: CreateRoleDto })
   @Post()
   create(@Body() dto: CreateRoleDto) {
     return this.rolesService.createRole(dto)
   }
 
+  @ApiOperation({ summary: "Get role by value" })
+  @ApiResponse({ status: 200, type: CreateRoleDto })
   @Get("/:value")
   getByValue(@Param("value") value: string) {
     return this.rolesService.getRoleByValue(value)
