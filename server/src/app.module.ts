4b99e59ec12ea56e1e98b5c7ae69cc93614924cc
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2024-12-20 13:38:02 +0200

    Reformat app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common"
 import { ConfigModule, ConfigService } from "@nestjs/config"
 import { SequelizeModule } from "@nestjs/sequelize"
 import { getSequelizeConfig } from "./config/database.config"
-import { UsersModule } from './users/users.module';
+import { UsersModule } from "./users/users.module"
 
 @Module({
   imports: [
