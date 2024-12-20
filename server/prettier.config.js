0f476d2a4c1b3e09cf0b1de79cdec6b061d48f4c
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2024-12-20 12:38:04 +0200

    Init nest project

---
+++ server/prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import("prettier").Config}
+ */
+
+module.exports = {
+  trailingComma: "none",
+  semi: false,
+  tabWidth: 2,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  arrowParens: "avoid"
+}
