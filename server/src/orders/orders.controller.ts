c96f364b8622d5a5e65bc9421fd6e68ac17cdc0d
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2024-12-21 17:17:58 +0200

    Apply orders module

--- server/src/orders/orders.controller.ts
+++ server/src/orders/orders.controller.ts
@@ -1,5 +1,38 @@
-import { Controller } from "@nestjs/common"
+import { Body, Controller, Get, Param, Post } from "@nestjs/common"
+import { OrdersService } from "./orders.service"
+import { ApiOperation, ApiResponse } from "@nestjs/swagger"
+import { CreateOrderDto } from "./dto/create-order.dto"
 
 @Controller("orders")
 export class OrdersController {
+  constructor(private ordersService: OrdersService) {
+  }
+
+  @ApiOperation({ summary: "Create order" })
+  @ApiResponse({ status: 200, type: CreateOrderDto })
+  @Post()
+  create(@Body() dto: CreateOrderDto) {
+    return this.ordersService.createOrder(dto)
+  }
+
+  @ApiOperation({ summary: "Get order by id" })
+  @ApiResponse({ status: 200, type: CreateOrderDto })
+  @Get("/:id")
+  getById(@Param("id") id: number) {
+    return this.ordersService.getOrdersById(id)
+  }
+
+  @ApiOperation({ summary: "Get all orders" })
+  @ApiResponse({ status: 200, type: Array<CreateOrderDto> })
+  @Get("/all")
+  getAll() {
+    return this.ordersService.getAllOrders()
+  }
+
+  @ApiOperation({ summary: "Get all user's orders" })
+  @ApiResponse({ status: 200, type: Array<CreateOrderDto> })
+  @Get("/all/:id")
+  getAllByUserID(@Param("id") id: number) {
+    return this.ordersService.getAllOrdersByUserId(id)
+  }
 }
--- server/src/orders/orders.service.ts
+++ server/src/orders/orders.service.ts
@@ -1,5 +1,26 @@
 import { Injectable } from "@nestjs/common"
+import { CreateOrderDto } from "./dto/create-order.dto"
+import { InjectModel } from "@nestjs/sequelize"
+import { Order } from "./orders.model"
 
 @Injectable()
 export class OrdersService {
+  constructor(@InjectModel(Order) private orderRepository: typeof Order) {
+  }
+
+  async createOrder(dto: CreateOrderDto) {
+    return await this.orderRepository.create(dto)
+  }
+
+  async getOrdersById(id: number) {
+    return await this.orderRepository.findOne({ where: { id } })
+  }
+
+  async getAllOrdersByUserId(userId: number) {
+    return await this.orderRepository.findAll({ where: { userId } })
+  }
+
+  async getAllOrders() {
+    return await this.orderRepository.findAll()
+  }
 }
