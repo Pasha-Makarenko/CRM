c709a73a7278cc94cb0bd22749419a1a43e2afb2
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2024-12-24 22:43:43 +0200

    Fix imports again

--- server/src/orders/orders.module.ts
+++ server/src/orders/orders.module.ts
@@ -1,16 +1,18 @@
-import { Module } from "@nestjs/common"
+import { forwardRef, Module } from "@nestjs/common"
 import { OrdersController } from "./orders.controller"
 import { OrdersService } from "./orders.service"
 import { SequelizeModule } from "@nestjs/sequelize"
 import { User } from "../users/users.model"
 import { Order } from "./orders.model"
 import { UsersModule } from "../users/users.module"
+import { AuthModule } from "../auth/auth.module"
 
 @Module({
   controllers: [ OrdersController ],
   providers: [ OrdersService ],
   imports: [
     SequelizeModule.forFeature([ Order, User ]),
+    AuthModule,
     UsersModule
   ],
   exports: [ OrdersService ]
