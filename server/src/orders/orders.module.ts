91a865fac30e97dca517508433445e11565506a1
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2024-12-24 22:42:45 +0200

    Fix imports

--- server/src/orders/orders.module.ts
+++ server/src/orders/orders.module.ts
@@ -4,14 +4,14 @@ import { OrdersService } from "./orders.service"
 import { SequelizeModule } from "@nestjs/sequelize"
 import { User } from "../users/users.model"
 import { Order } from "./orders.model"
-import { UsersService } from "../users/users.service"
+import { UsersModule } from "../users/users.module"
 
 @Module({
   controllers: [ OrdersController ],
   providers: [ OrdersService ],
   imports: [
     SequelizeModule.forFeature([ Order, User ]),
-    UsersService
+    UsersModule
   ],
   exports: [ OrdersService ]
 })
