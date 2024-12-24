02da1312937ee662e7d56e683d9f320d054259ab
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2024-12-24 22:44:28 +0200

    Remove unused function

--- server/src/orders/orders.module.ts
+++ server/src/orders/orders.module.ts
@@ -1,4 +1,4 @@
-import { forwardRef, Module } from "@nestjs/common"
+import { Module } from "@nestjs/common"
 import { OrdersController } from "./orders.controller"
 import { OrdersService } from "./orders.service"
 import { SequelizeModule } from "@nestjs/sequelize"
